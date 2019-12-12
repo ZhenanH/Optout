91a28f0d926e5ff2c7df960dd7a98f6b5ab8a8f2
Zhenan Hong <guoer23@gmail.com>
2019-12-12 16:09:37 -0500

    Updated with form submit

--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ import {
 import "antd/dist/antd.css";
 import "./index.css";
 
-function hasErrors(fieldsError) {
-  return Object.keys(fieldsError).some(field => fieldsError[field]);
-}
+// function hasErrors(fieldsError) {
+//   return Object.keys(fieldsError).some(field => fieldsError[field]);
+// }
 
 class HorizontalLoginForm extends React.Component {
+  state = {
+    isVerified: false,
+    agreedWithTerms: false,
+    submitted: false
+  };
+
   componentDidMount() {
     // To disabled submit button at the beginning.
-    this.props.form.validateFields();
+    //this.props.form.validateFields();
   }
 
   handleSubmit = e => {
@@ -23,158 +29,157 @@ class HorizontalLoginForm extends React.Component {
     this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log("Received values of form: ", values);
+        this.setState({ submitted: true });
+      } else {
+        console.log("err: ", err);
       }
     });
   };
 
+  checkCheckBox = (rule, value, callback) => {
+    console.log(value);
+    if (!value) {
+      callback("Please agree the terms and conditions!");
+    } else {
+      callback();
+    }
+  };
+
+  onVerify = token => {
+    // const Url = "https://www.google.com/recaptcha/api/siteverify";
+    // const Pram = {
+    //   method:"POST",
+    //   secret:"6LdhTccUAAAAAAmEOq3IpLk2UINqNhYTdzZNrIau",
+    //   response:token
+    // };
+
+    // fetch(Url,Pram)
+    // .then(data=>{return data.json()})
+    // .then(res=>{console.log(res)})
+    // .catch(error=>console.log(error));
+    console.log("token: ", token);
+    this.setState({ isVerified: true });
+  };
+
   render() {
     const {
-      getFieldDecorator,
-      getFieldsError,
-      getFieldError,
-      isFieldTouched
+      getFieldDecorator
+      //getFieldsError,
+      // getFieldError,
+      // isFieldTouched
     } = this.props.form;
 
-    // Only show error after a field is touched.
-    const usernameError =
-      isFieldTouched("username") && getFieldError("username");
-    const passwordError =
-      isFieldTouched("password") && getFieldError("password");
     return (
-      <GoogleReCaptchaProvider>
-        <Form layout="horizontal" onSubmit={this.handleSubmit}>
-          <Row gutter={32}>
-            <Col span={12}>
-              <Form.Item
-                label="First Name"
-                validateStatus={usernameError ? "error" : ""}
-                help={usernameError || ""}
-              >
-                {getFieldDecorator("fistname", {
-                  rules: [
-                    { required: true, message: "Please input your first name!" }
-                  ]
-                })(<Input placeholder="Enter first Name" />)}
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                label="Last Name"
-                validateStatus={passwordError ? "error" : ""}
-                help={passwordError || ""}
-              >
-                {getFieldDecorator("lastname", {
-                  rules: [
-                    { required: true, message: "Please input your last name!" }
-                  ]
-                })(<Input placeholder="Enter last name" />)}
-              </Form.Item>
-            </Col>
-          </Row>
+      <GoogleReCaptchaProvider reCaptchaKey="6LdhTccUAAAAAAmEOq3IpLk2UINqNhYTdzZNrIau">
+        <GoogleReCaptcha onVerify={this.onVerify} />
+        {this.state.submitted ? (
+          <div> Thanks you </div>
+        ) : (
+          <Form layout="horizontal" onSubmit={this.handleSubmit}>
+            <Row gutter={32}>
+              <Col span={12}>
+                <Form.Item label="First Name">
+                  {getFieldDecorator("fistname", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "Please enter your first name!"
+                      }
+                    ]
+                  })(<Input placeholder="Enter first Name" />)}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Last Name">
+                  {getFieldDecorator("lastname", {
+                    rules: [
+                      {
+                        required: true,
+                        message: "Please enter your last name!"
+                      }
+                    ]
+                  })(<Input placeholder="Enter last name" />)}
+                </Form.Item>
+              </Col>
+            </Row>
 
-          <Row gutter={32}>
-            <Col span={12}>
-              <Form.Item
-                label="Email"
-                validateStatus={usernameError ? "error" : ""}
-                help={usernameError || ""}
-              >
-                {getFieldDecorator("email", {
-                  rules: [
-                    { required: true, message: "Please input your first name!" }
-                  ]
-                })(<Input placeholder="Enter email" />)}
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                label="Address 1"
-                validateStatus={passwordError ? "error" : ""}
-                help={passwordError || ""}
-              >
-                {getFieldDecorator("address1", {
-                  rules: [
-                    { required: true, message: "Please input your last name!" }
-                  ]
-                })(<Input placeholder="Enter address 1" />)}
-              </Form.Item>
-            </Col>
-          </Row>
+            <Row gutter={32}>
+              <Col span={12}>
+                <Form.Item label="Email">
+                  {getFieldDecorator("email", {
+                    rules: []
+                  })(<Input placeholder="Enter email" />)}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Address 1">
+                  {getFieldDecorator("address1", {
+                    rules: [{ required: true, message: "Please enter address" }]
+                  })(<Input placeholder="Enter address 1" />)}
+                </Form.Item>
+              </Col>
+            </Row>
 
-          <Row gutter={32}>
-            <Col span={12}>
-              <Form.Item
-                label="Address 2"
-                validateStatus={usernameError ? "error" : ""}
-                help={usernameError || ""}
-              >
-                {getFieldDecorator("address2", {
-                  rules: [
-                    { required: true, message: "Please input your first name!" }
-                  ]
-                })(<Input placeholder="Enter address 2" />)}
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                label="City"
-                validateStatus={passwordError ? "error" : ""}
-                help={passwordError || ""}
-              >
-                {getFieldDecorator("city", {
-                  rules: [
-                    { required: true, message: "Please input your last name!" }
-                  ]
-                })(<Input placeholder="Enter city" />)}
-              </Form.Item>
-            </Col>
-          </Row>
+            <Row gutter={32}>
+              <Col span={12}>
+                <Form.Item label="Address 2">
+                  {getFieldDecorator("address2", {
+                    rules: []
+                  })(<Input placeholder="Enter address 2" />)}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="City">
+                  {getFieldDecorator("city", {
+                    rules: [{ required: true, message: "Please enter city" }]
+                  })(<Input placeholder="Enter city" />)}
+                </Form.Item>
+              </Col>
+            </Row>
 
-          <Row gutter={32}>
-            <Col span={12}>
-              <Form.Item
-                label="State"
-                validateStatus={usernameError ? "error" : ""}
-                help={usernameError || ""}
+            <Row gutter={32}>
+              <Col span={12}>
+                <Form.Item label="State">
+                  {getFieldDecorator("state", {
+                    rules: [{ required: true, message: "Please enter state" }]
+                  })(<Input placeholder="Enter state" />)}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Zip">
+                  {getFieldDecorator("zip", {
+                    rules: [{ required: true, message: "Please enter zipcode" }]
+                  })(<Input placeholder="Enter zip" />)}
+                </Form.Item>
+              </Col>
+            </Row>
+            <Form.Item>
+              {getFieldDecorator("agree", {
+                rules: [{ validator: this.checkCheckBox }]
+              })(
+                <Checkbox
+                  onChange={e =>
+                    this.setState({ agreedWithTerms: e.target.checked })
+                  }
+                >
+                  {" "}
+                  Placeholder for [legal terms here to comply with any
+                  regulatory requirements]
+                </Checkbox>
+              )}
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                //disabled={!this.agreedWithTerms}
               >
-                {getFieldDecorator("state", {
-                  rules: [
-                    { required: true, message: "Please input your first name!" }
-                  ]
-                })(<Input placeholder="Enter state" />)}
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item
-                label="Zip"
-                validateStatus={passwordError ? "error" : ""}
-                help={passwordError || ""}
-              >
-                {getFieldDecorator("zip", {
-                  rules: [
-                    { required: true, message: "Please input your last name!" }
-                  ]
-                })(<Input placeholder="Enter zip" />)}
-              </Form.Item>
-            </Col>
-          </Row>
-          <Form.Item>
-            <div>
-              <Checkbox>
-                Placeholder for [legal terms here to comply with any regulatory
-                requirements]
-              </Checkbox>
-            </div>
-            <GoogleReCaptcha onVerify={token => console.log(token)} />
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={hasErrors(getFieldsError())}
-            >
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        )}
       </GoogleReCaptchaProvider>
     );
   }
