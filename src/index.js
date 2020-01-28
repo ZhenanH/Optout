0f3e1e5fa09f3a86eaaec26359091c2f127e1c11
Zhenan Hong <guoer23@gmail.com>
2020-01-28 09:59:04 -0500

    production API endpoint

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const { Option } = Select;
 const states = "Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware, Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana, Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana, Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina, North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina, South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia, Wisconsin, Wyoming".split(
   ","
 );
-const endpoint = "https://optout.pbbl.co/prod";
+const endpoint = "https://optout.pbbl.co/v1/optout";
 //const endpoint ="https://m7pwfi5u15.execute-api.us-east-1.amazonaws.com/develop";
 // function hasErrors(fieldsError) {
 //   return Object.keys(fieldsError).some(field => fieldsError[field]);
